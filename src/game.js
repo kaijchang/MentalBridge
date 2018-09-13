d1e1c14a72bfe569162372df8ceb6648eb90f614
Kai Chang <kaijchang@gmail.com>
2018-09-12 21:25:23 -0700

    reactstrap

---
+++ src/game.js
@@ -0,0 +1,217 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+
+import * as mp from "mental-poker";
+import shuffle from "lodash.shuffle";
+
+import arrayBufferToBuffer from "arraybuffer-to-buffer";
+
+import { subscribeToGameStart, subscribeToPositions, subscribeToJoin, subscribeToLeave, subscribeToCodeWords, sendCodeWords, subscribeToShuffledDeck, sendShuffledDeck, subscribeToLockedDeck, sendLockedDeck } from "./sockets";
+
+const cardinalMap = {
+	"North": "East",
+	"East": "South",
+	"South": "West",
+	"West": "North"
+};
+
+class Player extends React.Component {
+	render() {
+		return (
+			<Col>
+				{this.props.position} - {this.props.status}
+			</Col>
+		)
+	}
+}
+
+export default class Game extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			gameStatus: "Waiting for Players",
+			config: mp.createConfig(52),
+			turn: "",
+			playerPosition: "",
+			self: {},
+			deck: [],
+			North: {
+				codeWords: [],
+				status: "Empty"
+			},
+			East: {
+				codeWords: [],
+				status: "Empty"
+			},
+			South: {
+				codeWords: [],
+				status: "Empty"
+			},
+			West: {
+				codeWords: [],
+				status: "Empty"
+			}
+		};
+
+		subscribeToPositions((otherPlayers, playerPosition) => {
+			this.setState({
+				playerPosition: playerPosition,
+  				[playerPosition]: Object.assign({}, this.state[playerPosition], {status: "Self"})
+			});
+
+			otherPlayers.forEach(pos => {
+				this.setState({
+  					[pos]: Object.assign({}, this.state[pos], {status: "Occupied"})
+				});
+			});
+		});
+
+		subscribeToGameStart(() => {
+			if (this.state.gameStatus === "Waiting for Players") {
+				this.setState({
+					gameStatus: "Sending Codewords",
+					self: mp.createPlayer(this.state.config)
+				});
+
+				console.log(this.state.gameStatus);
+
+				subscribeToCodeWords((codeWords, playerPosition) => {
+					if (this.state[playerPosition].codeWords.length === 0) {
+						this.setState({
+							[playerPosition]: Object.assign({}, this.state[playerPosition], {codeWords: codeWords.map(codeWord => arrayBufferToBuffer(codeWord))})
+						});
+
+						if (this.state.North.codeWords.length === 52 && this.state.East.codeWords.length === 52 && this.state.South.codeWords.length === 52 && this.state.West.codeWords.length === 52) {
+							this.setState({
+								gameStatus: "Shuffling",
+								deck: mp.createDeck([this.state.North.codeWords, this.state.East.codeWords, this.state.South.codeWords, this.state.West.codeWords]),
+								turn: "North"
+							});
+
+							console.log(this.state.gameStatus);
+
+							subscribeToShuffledDeck((shuffledDeck, playerPosition) => {
+								if (this.state.gameStatus === "Shuffling" && playerPosition === this.state.turn) {
+									this.setState({
+										deck: shuffledDeck.map(card => arrayBufferToBuffer(card)),
+										turn: cardinalMap[this.state.turn]
+									});
+
+									if (this.state.turn === "North") {
+										this.setState({
+											gameStatus: "Locking"
+										});
+
+										console.log(this.state.gameStatus);
+
+										subscribeToLockedDeck((lockedDeck, playerPosition) => {
+											if (this.state.gameStatus === "Locking" && playerPosition === this.state.turn) {
+												this.setState({
+													deck: lockedDeck.map(card => arrayBufferToBuffer(card)),
+													turn: cardinalMap[this.state.turn]
+												});
+
+												if (this.state.turn === "North") {
+													this.setState({
+														gameStatus: "Dealing"
+													});
+
+													console.log(this.state.gameStatus);
+
+												} else if (this.state.turn === this.state.playerPosition) {
+													this.setState({
+														deck: mp.encryptDeck(
+  																mp.decryptDeck(this.state.deck, this.state.self.keyPairs[this.state.config.cardCount].privateKey),
+  																this.state.self.keyPairs.map(keyPair => keyPair.privateKey),
+													  	)
+													});
+
+													sendLockedDeck(this.state.deck);
+												}
+											}
+										});
+
+										if (this.state.turn === this.state.playerPosition) {
+											this.setState({
+												deck: mp.encryptDeck(
+  														mp.decryptDeck(this.state.deck, this.state.self.keyPairs[this.state.config.cardCount].privateKey),
+  														this.state.self.keyPairs.map(keyPair => keyPair.privateKey),
+												)
+											});
+
+											sendLockedDeck(this.state.deck);
+										}
+
+									} else if (this.state.turn === this.state.playerPosition) {
+										this.setState({
+											deck: mp.encryptDeck(shuffle(this.state.deck), this.state.self.keyPairs[this.state.config.cardCount].privateKey)
+										});
+
+										sendShuffledDeck(this.state.deck);
+									}
+								}
+							});
+
+							if (this.state.turn === this.state.playerPosition) {
+								this.setState({
+									deck: mp.encryptDeck(shuffle(this.state.deck), this.state.self.keyPairs[this.state.config.cardCount].privateKey)
+								});
+
+								sendShuffledDeck(this.state.deck);
+							}
+						}
+					}
+				});
+
+				sendCodeWords(this.state.self.cardCodewordFragments);
+			}
+		});
+
+		subscribeToJoin((pos) => {
+			this.setState({
+  				[pos]: Object.assign({}, this.state[pos], {status: "Occupied"})
+			});
+		});
+
+		subscribeToLeave((pos) => {
+			this.setState({
+  				[pos]: Object.assign({}, this.state[pos], {status: "Empty"})
+			});
+		});
+	}
+
+	render() {
+		return (
+			<Container>
+				<Row>
+					<Col></Col>
+					<Player
+						position="North"
+						status={this.state.North.status}
+					/>
+					<Col></Col>
+				</Row>
+				<Row>
+					<Player
+						position="West"
+						status={this.state.West.status}
+					/>
+					<Col></Col>
+					<Player
+						position="East"
+						status={this.state.East.status}
+					/>
+				</Row>
+				<Row>
+					<Col></Col>
+					<Player
+						position="South"
+						status={this.state.South.status}
+					/>
+					<Col></Col>
+				</Row>
+			</Container>
+		)
+	}
+}
--- src/sockets.js
+++ src/sockets.js
@@ -3,41 +3,41 @@ var socket = openSocket("http://localhost:8000");
 
 // subscribe
 
-function subscribeToGameStart(callback) {
+export function subscribeToGameStart(callback) {
 	socket.on("start", callback);
 }
 
-function subscribeToPositions(callback) {
+export function subscribeToPositions(callback) {
 	socket.on("positions", (otherPlayers, playerPosition) => {
 		callback(otherPlayers, playerPosition);
 	});
 }
 
-function subscribeToJoin(callback) {
+export function subscribeToJoin(callback) {
 	socket.on("join", (pos) => {
 		callback(pos);
 	});
 }
 
-function subscribeToLeave(callback) {
+export function subscribeToLeave(callback) {
 	socket.on("leave", (pos) => {
 		callback(pos);
 	});
 }
 
-function subscribeToCodeWords(callback) {
+export function subscribeToCodeWords(callback) {
 	socket.on("codeWords", (codeWords, playerPosition) => {
 		callback(codeWords, playerPosition);
 	});
 }
 
-function subscribeToShuffledDeck(callback) {
+export function subscribeToShuffledDeck(callback) {
 	socket.on("shuffledDeck", (shuffledDeck, playerPosition) => {
 		callback(shuffledDeck, playerPosition);
 	});
 }
 
-function subscribeToLockedDeck(callback) {
+export function subscribeToLockedDeck(callback) {
 	socket.on("lockedDeck", (lockedDeck, playerPosition) => {
 		callback(lockedDeck, playerPosition);
 	});
@@ -45,16 +45,14 @@ function subscribeToLockedDeck(callback) {
 
 // send
 
-function sendCodeWords(codeWords) {
+export function sendCodeWords(codeWords) {
 	socket.emit("codeWords", codeWords);
 }
 
-function sendShuffledDeck(shuffledDeck) {
+export function sendShuffledDeck(shuffledDeck) {
 	socket.emit("shuffledDeck", shuffledDeck);
 }
 
-function sendLockedDeck(lockedDeck) {
+export function sendLockedDeck(lockedDeck) {
 	socket.emit("lockedDeck", lockedDeck);
 }
-
-export { subscribeToGameStart, subscribeToPositions, subscribeToJoin, subscribeToLeave, subscribeToCodeWords, sendCodeWords, subscribeToShuffledDeck, sendShuffledDeck, subscribeToLockedDeck, sendLockedDeck };
